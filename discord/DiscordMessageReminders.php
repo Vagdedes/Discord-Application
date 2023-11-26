6f47621267187a8f73d1c08811020c122af335da
Evangelos <contact@vagdedes.com>
2023-11-26 23:46:31 +0200

    Added embed support to message reminders

--- discord/DiscordMessageReminders.php
+++ discord/DiscordMessageReminders.php
@@ -48,17 +48,18 @@ class DiscordMessageReminders
 
     private function execute(Channel|Thread $channel, object $row, $checkPrevious = true): void
     {
-        if (empty(get_sql_query(
-            BotDatabaseTable::BOT_MESSAGE_REMINDER_TRACKING,
-            array("id"),
-            array(
-                array("deletion_date", null),
-                array("reminder_id", $row->id),
-                array("creation_date", ">", get_past_date($row->cooldown)),
-            ),
-            null,
-            1
-        ))) {
+        if (!$checkPrevious
+            || empty(get_sql_query(
+                BotDatabaseTable::BOT_MESSAGE_REMINDER_TRACKING,
+                array("id"),
+                array(
+                    array("deletion_date", null),
+                    array("reminder_id", $row->id),
+                    array("creation_date", ">", get_past_date($row->cooldown)),
+                ),
+                null,
+                1
+            ))) {
             if ($checkPrevious && $row->check_previous !== null) {
                 $channel->getMessageHistory(array("limit" => (int)$row->check_previous))->done(
                     function (Collection $messages) use ($channel, $row) {
@@ -73,7 +74,53 @@ class DiscordMessageReminders
                     }
                 );
             } else {
-                $channel->sendMessage(MessageBuilder::new()->setContent($row->message_content))->done(
+                $messageBuilder = MessageBuilder::new()->setContent(
+                    empty($messageRow->message_content) ? ""
+                        : $messageRow->message_content
+                );
+                $embed = new Embed($this->plan->discord);
+                $addEmbed = false;
+
+                if (!empty($messageRow->embed_title)) {
+                    $embed->setTitle($messageRow->embed_title);
+                    $addEmbed = true;
+                }
+                if (!empty($messageRow->embed_description)) {
+                    $embed->setDescription($messageRow->embed_description);
+                    $addEmbed = true;
+                }
+                if (!empty($messageRow->embed_url)) {
+                    $embed->setUrl($messageRow->embed_url);
+                    $addEmbed = true;
+                }
+                if ($messageRow->embed_color !== null) {
+                    $embed->setColor($messageRow->embed_color);
+                    $addEmbed = true;
+                }
+                if ($messageRow->embed_image !== null) {
+                    $embed->setImage($messageRow->embed_image);
+                    $addEmbed = true;
+                }
+                if ($messageRow->embed_timestamp !== null) {
+                    $embed->setTimestamp(strtotime($messageRow->embed_timestamp));
+                    $addEmbed = true;
+                }
+                if ($messageRow->embed_footer !== null) {
+                    $embed->setFooter($messageRow->embed_footer);
+                    $addEmbed = true;
+                }
+                if (!empty($messageRow->embed_author_name)) {
+                    $embed->setAuthor(
+                        $messageRow->embed_author_name,
+                        $messageRow->embed_author_icon_url,
+                        $messageRow->embed_author_url,
+                    );
+                    $addEmbed = true;
+                }
+                if ($addEmbed) {
+                    $messageBuilder->addEmbed($embed);
+                }
+                $channel->sendMessage($messageBuilder)->done(
                     function (Message $message) use ($row) {
                         if (sql_insert(
                             BotDatabaseTable::BOT_MESSAGE_REMINDER_TRACKING,
@@ -82,7 +129,7 @@ class DiscordMessageReminders
                                 "channel_id" => $message->channel_id,
                                 "thread_id" => $message->thread?->id,
                                 "message_id" => $message->id,
-                                "message_content" => $row->message_content,
+                                "message_object" => json_encode($message->getRawAttributes()),
                                 "creation_date" => get_current_date()
                             ),
                         )) {
