67c87a29bf983c0d0328a3a353b72c708727d8ac
Evangelos <contact@vagdedes.com>
2023-11-28 11:54:03 +0200

    Modified invite tracker to be more functional

--- discord/DiscordInviteTracker.php
+++ discord/DiscordInviteTracker.php
@@ -1,5 +1,7 @@
 <?php
 
+use Discord\Parts\Guild\Guild;
+
 class DiscordInviteTracker
 {
     private DiscordPlan $plan;
@@ -13,61 +15,66 @@ class DiscordInviteTracker
             self::$isInitialized = true;
 
             foreach ($this->plan->discord->guilds as $guild) {
-                if (!empty($guild->invites->toArray())) {
-                    foreach ($guild->invites as $invite) {
-                        $totalUses = $invite->uses ?? null;
-                        $code = $invite->code;
-                        $serverID = $invite->guild_id;
+                $this->track($guild);
+            }
+        }
+    }
 
-                        if ($totalUses !== null && $code !== null && $serverID !== null) {
-                            $query = get_sql_query(
+    private function track(Guild $guild): void
+    {
+        $guild->getInvites()->done(function (mixed $invites) {
+            foreach ($invites as $invite) {
+                $totalUses = $invite->uses ?? null;
+                $code = $invite->code ?? null;
+                $serverID = $invite->guild_id ?? null;
+
+                if ($totalUses !== null && $code !== null && $serverID !== null) {
+                    $query = get_sql_query(
+                        BotDatabaseTable::BOT_INVITE_TRACKER,
+                        array("id", "individual_uses", "total_uses"),
+                        array(
+                            array("server_id", $serverID),
+                            array("invite_code", $code),
+                            array("deletion_date", null)
+                        ),
+                        array(
+                            "DESC",
+                            "id"
+                        ),
+                        1
+                    );
+
+                    if (empty($query)) {
+                        sql_insert(
+                            BotDatabaseTable::BOT_INVITE_TRACKER,
+                            array(
+                                "server_id" => $serverID,
+                                "user_id" => $invite->inviter?->id,
+                                "invite_code" => $code,
+                                "individual_uses" => $totalUses,
+                                "total_uses" => $totalUses,
+                                "creation_date" => get_current_date()
+                            )
+                        );
+                    } else {
+                        $difference = $totalUses - $query[0]->total_uses;
+
+                        if ($difference > 0) {
+                            sql_insert(
                                 BotDatabaseTable::BOT_INVITE_TRACKER,
-                                array("id", "individual_uses", "total_uses"),
                                 array(
-                                    array("server_id", $serverID),
-                                    array("invite_code", $code),
-                                    array("deletion_date", null)
-                                ),
-                                array(
-                                    "DESC",
-                                    "id"
-                                ),
-                                1
+                                    "server_id" => $serverID,
+                                    "user_id" => $invite->inviter?->id,
+                                    "invite_code" => $code,
+                                    "individual_uses" => $difference,
+                                    "total_uses" => $totalUses,
+                                    "creation_date" => get_current_date()
+                                )
                             );
-
-                            if (empty($query)) {
-                                sql_insert(
-                                    BotDatabaseTable::BOT_INVITE_TRACKER,
-                                    array(
-                                        array("server_id", $serverID),
-                                        array("user_id", $invite->inviter?->id),
-                                        array("invite_code", $code),
-                                        array("individual_uses", $totalUses),
-                                        array("total_uses", $totalUses),
-                                        array("creation_date", get_current_date()),
-                                    )
-                                );
-                            } else {
-                                $difference = $totalUses - $query[0]->total_uses;
-
-                                if ($difference > 0) {
-                                    sql_insert(
-                                        BotDatabaseTable::BOT_INVITE_TRACKER,
-                                        array(
-                                            array("server_id", $invite->guild_id),
-                                            array("user_id", $invite->inviter?->id),
-                                            array("invite_code", $invite->code),
-                                            array("individual_uses", $difference),
-                                            array("total_uses", $totalUses),
-                                            array("creation_date", get_current_date()),
-                                        )
-                                    );
-                                }
-                            }
                         }
                     }
                 }
             }
-        }
+        });
     }
 }
