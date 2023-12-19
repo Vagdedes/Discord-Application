66d6e6c28a1820c1300fc9bfbeb55efb43c4d1db
Evangelos <contact@vagdedes.com>
2023-12-19 20:18:58 +0200

    Made progress in the user polls functionality

--- discord/user/standalone/DiscordUserPolls.php
+++ discord/user/standalone/DiscordUserPolls.php
@@ -8,7 +8,12 @@ class DiscordUserPolls
     private DiscordPlan $plan;
     private array $polls;
 
-    private const REFRESH_TIME = "15 seconds";
+    private const
+        REFRESH_TIME = "15 seconds",
+        MANAGE_PERMISSION = "idealistic.user.polls.manage",
+
+        NOT_EXISTS = "This user poll does not exist.",
+        NOT_OWNED = "You do not own this user poll.";
 
     public function __construct(DiscordPlan $plan)
     {
@@ -40,8 +45,29 @@ class DiscordUserPolls
 
         if ($get === null) {
             return "This user poll does not exist.";
+        } else if (!$this->owns($member, $get)) {
+            return self::NOT_OWNED;
+        } else {
+            $result = $this->endRaw($get);
+
+            if ($result !== null) {
+                return $result;
+            } else if (set_sql_query(
+                BotDatabaseTable::BOT_POLLS,
+                array(
+                    "deletion_date" => get_current_date()
+                ),
+                array(
+                    array("id", $get->id)
+                ),
+                null,
+                1
+            )) {
+                return null;
+            } else {
+                return "Failed to delete this user poll from the database.";
+            }
         }
-        return null;
     }
 
     // Separator
@@ -52,12 +78,13 @@ class DiscordUserPolls
         $get = $this->get($name);
 
         if ($get === null) {
-            return "This user poll does not exist.";
+            return self::NOT_EXISTS;
         } else if (!$this->owns($member, $get)) {
-            return "You do not own this user poll.";
+            return self::NOT_OWNED;
         } else if ($this->isRunning($get)) {
             return "This user poll is already running.";
         }
+        //todo
         return null;
     }
 
@@ -73,12 +100,18 @@ class DiscordUserPolls
         } else if ($this->isRunning($get)) {
             return "This user poll is already running.";
         }
+        return $this->endRaw($get);
+    }
+
+    public function endRaw(object $query): ?string
+    {
+        //todo
         return null;
     }
 
     private function finish(object $query): void
     {
-
+        //todo
     }
 
     // Separator
@@ -95,31 +128,109 @@ class DiscordUserPolls
 
     public function setPick(int|float|string $name, int|float|string $choice, bool $set = true): ?string
     {
+        if ($set) {
+
+        } else {
+
+        }
         return null;
     }
 
     // Separator
 
-    private function hasPermission(object $query): bool
+    private function getPermissions(object $query): array
     {
-        return false;
+        set_sql_cache("1 second");
+        return get_sql_query(
+            BotDatabaseTable::BOT_POLL_PERMISSIONS,
+            null,
+            array(
+                array("deletion_date", null),
+                array("poll_id", $query->id)
+            )
+        );
+    }
+
+    private function hasPermission(Member $member, object $query): bool
+    {
+        $permissions = $this->getPermissions($query);
+
+        if (!empty($permissions)) {
+            foreach ($permissions as $permission) {
+                if (!$this->plan->permissions->hasPermission($member, $permission->permission_id)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public function setRequiredPermission(int|float|string $name, int|string $permission, bool $set = true): ?string
     {
-        return null;
+        $query = $this->get($name);
+
+        if ($query === null) {
+            return self::NOT_EXISTS;
+        } else {
+            $permissions = $this->getPermissions($query);
+            //todo
+            return null;
+        }
     }
 
     // Separator
 
-    private function hasRequiredRole(object $query): bool
+    private function getRequiredRoles(object $query): array
     {
+        set_sql_cache("1 second");
+        return get_sql_query(
+            BotDatabaseTable::BOT_POLL_ROLES,
+            null,
+            array(
+                array("deletion_date", null),
+                array("poll_id", $query->id)
+            )
+        );
+    }
+
+    private function hasRequiredRole(Member $member, object $query): bool
+    {
+        $roles = $this->getRequiredRoles($query);
+
+        if (!empty($roles)) {
+            $memberRoles = $member->roles->toArray();
+
+            if (empty($memberRoles)) {
+                return false;
+            }
+            foreach ($roles as $role) {
+                $has = false;
+
+                foreach ($memberRoles as $memberRole) {
+                    if ($role->role_id == $memberRole->id) {
+                        $has = true;
+                        break;
+                    }
+                }
+                if (!$has) {
+                    return false;
+                }
+            }
+        }
         return false;
     }
 
     public function setRequiredRole(int|float|string $name, int|string $roleID, bool $set = true): ?string
     {
-        return null;
+        $query = $this->get($name);
+
+        if ($query === null) {
+            return self::NOT_EXISTS;
+        } else {
+            $roles = $this->getRequiredRoles($query);
+            //todo
+            return null;
+        }
     }
 
     // Separator
@@ -133,7 +244,17 @@ class DiscordUserPolls
 
     private function get(int|float|string $name): ?object
     {
-        return null;
+        $query = get_sql_query(
+            BotDatabaseTable::BOT_POLLS,
+            array("id"),
+            array(
+                array("deletion_date", null),
+                array("name", $name),
+            ),
+            null,
+            1
+        );
+        return empty($query) ? null : $query[0];
     }
 
     private function update(object $query): void
@@ -184,15 +305,7 @@ class DiscordUserPolls
     private function owns(Member $member, object $query): bool
     {
         set_sql_cache("1 second");
-        return !empty(get_sql_query(
-                BotDatabaseTable::BOT_POLLS,
-                array("id"),
-                array(
-                    array("id", $query->id),
-                    array("user_id", $member->id)
-                ),
-                null,
-                1
-            )) || $this->plan->permissions->hasPermission($member, "idealistic.user.polls.manage");
+        return $query->user_id == $member->id
+            || $this->plan->permissions->hasPermission($member, self::MANAGE_PERMISSION);
     }
 }
