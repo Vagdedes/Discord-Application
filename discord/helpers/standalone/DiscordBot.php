474f94fd03d21800375747b3c87a8830fe24cc9d
Evangelos <contact@vagdedes.com>
2024-02-05 09:19:16 +0200

    Removed certain caches from the SQL methods

--- discord/helpers/standalone/DiscordBot.php
+++ discord/helpers/standalone/DiscordBot.php
@@ -121,7 +121,7 @@ class DiscordBot
             reset_all_sql_connections();
             clear_memory(null);
 
-            if (false && $this->counter === 10) {
+            if ($this->counter === 10) {
                 $this->counter = 0;
                 $this->discord->close(true);
                 initiate_discord_bot();
