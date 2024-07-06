b97e5cbc0ca61b0a352567c983c84e140ed9c63f
Evangelos <contact@vagdedes.com>
2024-07-06 21:09:33 +0300

    Added give product command

--- listeners/implementation/command/7/CommandImplementationListener.php
+++ listeners/implementation/command/7/CommandImplementationListener.php
@@ -11,28 +11,56 @@ class CommandImplementationListener
                                      object      $command): void
     {
         $message = new MessageBuilder();
+        $account = new Account($plan->applicationID);
+        $account->getSession()->setCustomKey("discord", $interaction->data?->resolved?->users?->first()?->id);
+        $object = $account->getSession()->getLastKnown();
 
-        try {
-            $account = new Account($plan->applicationID);
-            $account->getSession()->setCustomKey("discord", $interaction->data?->resolved?->users?->first()?->id);
-            $object = $account->getSession()->getLastKnown();
+        if ($object !== null) {
+            $account = $account->getNew($object->account_id);
 
-            if ($object !== null) {
-                $account = $account->getNew($object->account_id);
-
-                if ($account->exists()) {
-                    $message->setContent(
-                        "https://www.idealistic.ai/contents/?path=account/panel&platform=1&id="
-                        . $account->getDetail("email_address")
-                    );
-                } else {
-                    $message->setContent("Account not found.");
-                }
+            if ($account->exists()) {
+                $message->setContent(
+                    "https://www.idealistic.ai/contents/?path=account/panel&platform=1&id="
+                    . $account->getDetail("email_address")
+                );
             } else {
-                $message->setContent("Object not found.");
+                $message->setContent("Account not found.");
             }
-        } catch (Throwable $e) {
-            $message->setContent($e->getMessage() . " | " . $e->getLine());
+        } else {
+            $message->setContent("Object not found.");
+        }
+        $plan->utilities->acknowledgeCommandMessage(
+            $interaction,
+            $message,
+            true
+        );
+    }
+
+    public static function user_give_product(DiscordPlan $plan,
+                                             Interaction $interaction,
+                                             object      $command): void
+    {
+        $message = new MessageBuilder();
+        $account = new Account($plan->applicationID);
+        $account->getSession()->setCustomKey("discord", $interaction->data?->resolved?->users?->first()?->id);
+        $object = $account->getSession()->getLastKnown();
+
+        if ($object !== null) {
+            $account = $account->getNew($object->account_id);
+
+            if ($account->exists()) {
+                $arguments = $interaction->data->options->toArray();
+                $reply = $account->getPurchases()->add(
+                    $arguments["product-id"]["value"]
+                );
+                $message->setContent(
+                    ($reply->isPositiveOutcome() ? "true" : "false") . " | " . $reply->getMessage()
+                );
+            } else {
+                $message->setContent("Account not found.");
+            }
+        } else {
+            $message->setContent("Object not found.");
         }
         $plan->utilities->acknowledgeCommandMessage(
             $interaction,
