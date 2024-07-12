ec3a87738ed7ccb12567e044cc526537dc357a48
Evangelos <contact@vagdedes.com>
2024-07-13 01:00:23 +0300

    Added paypal commands to manage transactions

--- listeners/implementation/command/7/CommandImplementationListener.php
+++ listeners/implementation/command/7/CommandImplementationListener.php
@@ -44,8 +44,8 @@ class CommandImplementationListener
     }
 
     public static function give_product(DiscordPlan $plan,
-                                             Interaction $interaction,
-                                             object      $command): void
+                                        Interaction $interaction,
+                                        object      $command): void
     {
         $message = new MessageBuilder();
         $account = new Account($plan->applicationID);
@@ -75,8 +75,8 @@ class CommandImplementationListener
     }
 
     public static function exchange_product(DiscordPlan $plan,
-                                        Interaction $interaction,
-                                        object      $command): void
+                                            Interaction $interaction,
+                                            object      $command): void
     {
         $message = new MessageBuilder();
         $account = new Account($plan->applicationID);
@@ -125,6 +125,74 @@ class CommandImplementationListener
                     $arguments["permanent"]["value"]
                 );
                 $message->setContent(self::printResult($reply));
+            } else {
+                $message->setContent("Account not found.");
+            }
+        } else {
+            $message->setContent("Object not found.");
+        }
+        $plan->utilities->acknowledgeCommandMessage(
+            $interaction,
+            $message,
+            true
+        );
+    }
+
+    public static function queue_paypal_transaction(DiscordPlan $plan,
+                                                    Interaction $interaction,
+                                                    object      $command): void
+    {
+        $arguments = $interaction->data->options->toArray();
+        var_dump(strval(queue_paypal_transaction($arguments["transaction-id"]["value"])));
+    }
+
+    public static function fail_paypal_transaction(DiscordPlan $plan,
+                                                   Interaction $interaction,
+                                                   object      $command): void
+    {
+        $arguments = $interaction->data->options->toArray();
+        var_dump(strval(process_failed_paypal_transaction($arguments["transaction-id"]["value"])));
+    }
+
+    public static function suspend_paypal_transactions(DiscordPlan $plan,
+                                                       Interaction $interaction,
+                                                       object      $command): void
+    {
+        $message = new MessageBuilder();
+        $account = new Account($plan->applicationID);
+        $account->getSession()->setCustomKey("discord", $interaction->data?->resolved?->users?->first()?->id);
+        $object = $account->getSession()->getLastKnown();
+
+        if ($object !== null) {
+            $account = $account->getNew($object->account_id);
+
+            if ($account->exists()) {
+                $arguments = $interaction->data->options->toArray();
+                $transactions = $account->getTransactions()->getSuccessful(PaymentProcessor::PAYPAL);
+
+                if (empty($transactions)) {
+                    $message->setContent("No transactions available.");
+                } else {
+                    $reason = $arguments["reason"]["value"];
+                    $coverFees = $arguments["cover-fees"]["value"];
+                    $success = 0;
+                    $failure = 0;
+
+                    foreach ($transactions as $transaction) {
+                        if (suspend_paypal_transaction(
+                            $transaction->id,
+                            $reason,
+                            $coverFees
+                        )) {
+                            $success++;
+                        } else {
+                            $failure++;
+                        }
+                    }
+                    $message->setContent(
+                        "Success: $success | Failure: $failure | Total: " . sizeof($transactions)
+                    );
+                }
             } else {
                 $message->setContent("Account not found.");
             }
