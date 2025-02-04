bc4b33799ccaf50fd5a05a0fc6257ea2b4d331b9
Evangelos <vaggdedes@gmail.com>
2025-02-05 01:08:42 +0200

    Update DiscordAIMessages.php

--- discord/message/DiscordAIMessages.php
+++ discord/message/DiscordAIMessages.php
@@ -655,6 +655,7 @@ class DiscordAIMessages
                 case AIModelFamily::CHAT_GPT_PRO:
                 case AIModelFamily::OPENAI_O1:
                 case AIModelFamily::OPENAI_O1_MINI:
+                case AIModelFamily::OPENAI_O3_MINI:
                 case AIModelFamily::OPENAI_VISION:
                 case AIModelFamily::OPENAI_VISION_PRO:
                 case AIModelFamily::OPENAI_SOUND:
